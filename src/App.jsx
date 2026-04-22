8d333baa84089648d8eb39cd6acdaf9f95902f1e
realjerryhuang <huangjerry67@gmail.com>
2026-04-22 01:46:02 -0700

    Worked up to "Taking Turns."

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import * as React from 'react'
 import * as ReactBootstrap from 'react-bootstrap'
+import { useState } from 'react'
 
 const { Badge, Button, Card } = ReactBootstrap
 
-export default function App() {
-  const [name, setName] = React.useState('World')
+function Square({ value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
 
   return (
-    <div className="container py-4">
-      <Card className="starter-card shadow-sm">
-        <Card.Body className="p-4">
-          <h1 className="greeting display-6 fw-bold">Hello, {name}!</h1>
-          <p className="mb-3 text-secondary">
-            This starter is set up to match the React Essentials notes more closely.
-            For the assignment, build the tic-tac-toe tutorial in this file and leave
-            mounting to <code>src/main.jsx</code>.
-          </p>
-          <div className="d-flex gap-2 flex-wrap align-items-center">
-            <Button variant="primary" onClick={() => setName('CS 35L')}>
-              Set example name
-            </Button>
-            <Badge bg="secondary" pill>
-              ReactBootstrap ready
-            </Badge>
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
-  )
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
